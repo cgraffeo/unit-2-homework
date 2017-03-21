d59802a77b13c1675d7477dff3caee32f50ce38d
Chris Graffeo <cgraffeo1@gmail.com>
2017-03-21 16:26:43 -0700

    added verification to /signup

--- main.py
+++ main.py
@@ -1,49 +1,94 @@
 import os
+import re
 
 import jinja2
 import webapp2
 
-import string
-
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape = True)
+jinja_env = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+USERNAME_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
+def valid_username(username):
+    return USERNAME_RE.match(username)
+
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
     def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
+        self.write(self.render_str(template, **kw))
+
 
 class MainPage(Handler):
-  def get(self):
-    items = self.request.get_all("food")
-    self.render("shopping_list.html", items = items)
+    def get(self):
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items=items)
+
 
 class FizzBuzzHandler(Handler):
-  def get (self):
-      n = self.request.get('n', 0)
-      n = n and int(n)
-      self.render('fizzbuzz.html', n = n)
+    def get(self):
+        n = self.request.get('n', 0)
+        n = n and int(n)
+        self.render('fizzbuzz.html', n=n)
+
 
 class Rot (Handler):
-  def get (self):
-    self.render('rot.html')
-  def post (self):
-    rot13= ''
-    text = self.request.get('text')
-    if text:
-      rot13 = text.encode('rot13')
-    self.render('rot.html', text = rot13)
+    def get(self):
+        self.render('rot.html')
 
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+        self.render('rot.html', text=rot13)
+
+
+class SignUp (Handler):
+    def get(self):
+        self.render('signup.html', errors={})
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify_password = self.request.get('verify_password')
+        errors = {
+            "username": False,
+            "password": False
+        }
+
+        if not valid_username(username):
+            errors["username"] = True
+
+        if password != verify_password:
+            errors["password"] = True
+
+        if errors["username"] or errors["password"]:
+            return self.render('signup.html',
+                               username=username,
+                               password=password, errors=errors)
+        self.response.set_cookie('username', username, max_age=360, path='/',
+                                 domain=None)
+        self.redirect('/welcome')
+
+
+class Welcome (Handler):
+    def get(self):
+        username = self.request.cookies.get('username')
+        self.render('welcome.html', username=username)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-     ('/fizzbuzz', FizzBuzzHandler),
-     ('/rot13', Rot)
+    ('/fizzbuzz', FizzBuzzHandler),
+    ('/rot13', Rot),
+    ('/signup', SignUp),
+    ('/welcome', Welcome)
 ], debug=True)
