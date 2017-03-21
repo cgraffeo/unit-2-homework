441020a4ad7fa8d104e56a92d1a80e7ad7fb14e7
Chris Graffeo <cgraffeo1@gmail.com>
2017-03-21 13:21:06 -0700

    Added ROT13 encoder at /rot13

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import os
 import jinja2
 import webapp2
 
+import string
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape= True)
+                                autoescape = True)
 
-
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
-      t = jinja_env.get_template(template)
-      return t.render(params)
-
     def render(self, template, **kw):
-      self.write(self.render_str(template, **kw))
+        self.response.out.write(render_str(template, **kw))
 
 class MainPage(Handler):
   def get(self):
@@ -31,16 +31,19 @@ class FizzBuzzHandler(Handler):
       n = n and int(n)
       self.render('fizzbuzz.html', n = n)
 
-class ROT (Handler):
+class Rot (Handler):
   def get (self):
-    # c = self.post
     self.render('rot.html')
-
-
+  def post (self):
+    rot13= ''
+    text = self.request.get('text')
+    if text:
+      rot13 = text.encode('rot13')
+    self.render('rot.html', text = rot13)
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
      ('/fizzbuzz', FizzBuzzHandler),
-     ('/rot13', ROT)
+     ('/rot13', Rot)
 ], debug=True)
